d0196df9db3da5dd6634f38f5f097de8879f215c
checIt-lab <57136184+checIt-lab@users.noreply.github.com>
2020-07-03 17:00:20 -0400

    Modal 2

--- src/navigation/RootNavigation.js
+++ src/navigation/RootNavigation.js
@@ -6,7 +6,7 @@ import AuthNavigation from './AuthNavigation';
 import { AUTH, TABS, TABSS } from '../consts/ConstsNavigation';
 import TabsNavigation from './TabsNavigation';
 import HostNavigation from './HostNavigation';
-import { View, Picker, Text } from 'react-native';
+import { View, Picker, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { Appbar } from 'react-native-paper';
 import  Button from '../components/Button'
 const MainStack = createStackNavigator();
@@ -103,10 +103,117 @@ function ModalScreen({ navigation }) {
                   </Picker>
               </View>
               <View style={{marginVertical:20}}>
-              <Button titulo="Next"
-                navigation={{}}/>
+                                                      
+
+              <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('MyModal2')}>
+                  <Text style={styles.tituleButton}>Next</Text>
+              </TouchableOpacity>                              
               </View>
           </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    backgroundColor: "#009D99",
+    borderRadius: 6,
+    padding: 14,
+    margin:5,
+    marginHorizontal:20,
+  },
+  tituleButton:{
+      fontSize:15,
+      fontWeight:'bold',
+      color:'#fff'
+  },
+});
+
+function ModallScreen({navigation}) {
+  return (
+    <View style={{ flex: 1 }}>
+      <Appbar.Header style={{ backgroundColor: '#152727', }}>
+      <Appbar.Action icon="close" size={15} onPress={() => navigation.goBack()} />
+         <Appbar.Content title="Tell us about your van"  titleStyle={{ alignSelf: 'flex-end' }}/>
+        </Appbar.Header>
+
+        <View style={{flex:1,}}>
+        <Image
+        style={{  
+          flex: 1,
+          width: null,
+          height: null,
+          resizeMode: 'contain',}}
+        source={require('../../assets/imagen/Captura.jpg')}
+      />
+        </View>
+        <View style={{marginVertical:20}}>
+                                                      
+
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Modall3')}>
+                <Text style={styles.tituleButton}>Next</Text>
+            </TouchableOpacity>                              
+            </View>
+    </View>
+  );
+}
+
+function ModallLScreen({navigation}) {
+  return (
+    <View style={{ flex: 1 }}>
+      <Appbar.Header style={{ backgroundColor: '#152727', }}>
+      <Appbar.Action icon="close" size={15} onPress={() => navigation.goBack()} />
+         <Appbar.Content title="Tell us about your van"  titleStyle={{ alignSelf: 'flex-end' }}/>
+        </Appbar.Header>
+
+        <View style={{flex:1,}}>
+        <Image
+        style={{  
+          flex: 1,
+          width: null,
+          height: null,
+          resizeMode: 'contain',}}
+        source={require('../../assets/imagen/Captur2.jpg')}
+      />
+        </View>
+        <View style={{marginVertical:20}}>
+                                                      
+
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Modall4')}>
+            <Text style={styles.tituleButton}>Next</Text>
+        </TouchableOpacity>                              
+        </View>
+    </View>
+  );
+}
+
+
+function ModallLlScreen({navigation}) {
+  return (
+    <View style={{ flex: 1 }}>
+      <Appbar.Header style={{ backgroundColor: '#152727', }}>
+      <Appbar.Action icon="close" size={15} onPress={() => navigation.goBack()} />
+         <Appbar.Content title="Tell us about your van"  titleStyle={{ alignSelf: 'flex-end' }}/>
+        </Appbar.Header>
+
+        <View style={{flex:1,}}>
+        <Image
+        style={{  
+          flex: 1,
+          width: null,
+          height: null,
+          resizeMode: 'contain',}}
+        source={require('../../assets/imagen/Captur3.jpg')}
+      />
+        </View>
+        <View style={{marginVertical:20}}>
+                                                      
+
+                          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate(TABSS)}>
+                              <Text style={styles.tituleButton}>exit</Text>
+                          </TouchableOpacity>                              
+                          </View>
     </View>
   );
 }
@@ -147,6 +254,11 @@ const RootNavigation = () => {
             <RootStack.Navigator mode="modal" headerMode="none">
             <RootStack.Screen name="Main" component={MainStackScreen} />
             <RootStack.Screen name="MyModal" component={ModalScreen} />
+            <RootStack.Screen name="MyModal2" component={ModallScreen} />
+
+            <RootStack.Screen name="Modall3" component={ModallLScreen} />
+            <RootStack.Screen name="Modall4" component={ModallLlScreen} />
+
           </RootStack.Navigator>
         </NavigationContainer>
     )
